ae6b9aab3c838379408a0bb2daf1e3050293cb98
arjunalakshan <arjunalakshan@proton.me>
2025-01-09 21:36:06 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_cast/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CloudCast Weather App",
+      home: HomeScreen(),
+    );
+  }
+}
